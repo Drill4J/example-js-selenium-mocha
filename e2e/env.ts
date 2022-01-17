306415dab15b247ffa586251612abac8b4d3dacc
RomanDavlyatshin <davlyatshinra@gmail.com>
2022-01-17 23:46:42 +0400

    drill4j + mocha integration

--- e2e/env.ts
+++ e2e/env.ts
@@ -1,16 +1,7 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// console.log('process.env', process.env.APP_URL);
-
 const env = {
-  // APP_URL: 'https://todomvc.com/examples/typescript-angular/',
-  APP_URL: 'http://host.docker.internal:8080',
+  APP_URL: 'http://host.docker.internal:3000',
   SELENIUM_HUB_URL: 'http://localhost:4444/wd/hub',
-  CHROMESTICK_URL: 'http://localhost:8093',
-  CDP_HOST: 'localhost',
-  CDP_PORT: '9222',
+  DEVTOOLS_PROXY_URL: 'http://localhost:8093',
   ADMIN_URL: 'http://localhost:8090',
   AGENT_ID: 'todomvc-typescript-angular',
 };
--- e2e/hooks.ts
+++ e2e/hooks.ts
@@ -1,77 +1,127 @@
 import { Builder, Capabilities, WebDriver } from 'selenium-webdriver';
 import { exec } from 'child_process';
 import os from 'os';
+import { Drill, DrillOptions } from '@drill4j/js-auto-test-agent';
 import axios from 'axios';
-import createDrillAutoTestAgent, { AutotestAgent } from '@drill4j/js-auto-test-agent';
+import { Runnable } from 'mocha';
+// import { AdminAPI } from '@drill4j/js-auto-test-agent/dist/admin-connect/types';
+let x: DrillOptions; // that seems to work, but line above does not
 
-let cdpSessionId;
-let drillBackend: AutotestAgent;
-
-declare global {
-  namespace NodeJS {
-    interface Global {
-      driver: WebDriver;
-    }
-  }
-}
+let drill: Drill;
 
 export const mochaHooks = {
-  // runs FOR EVERY FILE in parallel mode, and ONCE in serial mode
   async beforeAll() {
-    global.driver = await prepareWebdriver();
+    this.timeout(0); // for debug only
 
-    //--------------------------------- 3
-    cdpSessionId = (await axios.post(`${process.env.CHROMESTICK_URL}/sessions`, { host: process.env.CDP_HOST, port: process.env.CDP_PORT }))
-      .data;
-    await axios.post(`${process.env.CHROMESTICK_URL}/sessions/${cdpSessionId}/profiler/enable`);
-    await axios.post(`${process.env.CHROMESTICK_URL}/sessions/${cdpSessionId}/profiler/start-precise-coverage`, {
-      callCount: false,
-      detailed: true,
+    const chromeCapabilities = Capabilities.chrome();
+    chromeCapabilities.set('chromeOptions', {
+      args: [
+        // not important, used only for example
+        '--headless',
+        // these are very important, you likely don't wanna change them
+        '--remote-debugging-port=9222',
+        '--remote-debugging-address=0.0.0.0',
+      ],
     });
 
-    drillBackend = await createDrillAutoTestAgent({
+    global.driver = await new Builder()
+      .forBrowser('chrome')
+      .usingServer(process.env.SELENIUM_HUB_URL)
+      .withCapabilities(chromeCapabilities)
+      .build();
+
+    const res = await axios.get(`http://localhost:9222/json/version`);
+    const webSocketDebuggerUrl = (res.data.webSocketDebuggerUrl as string).replace('localhost', 'host.docker.internal');
+
+    drill = new Drill({
       adminUrl: process.env.ADMIN_URL,
+      devtoolsProxyUrl: process.env.DEVTOOLS_PROXY_URL,
+
+      // Agent's ID
       agentId: process.env.AGENT_ID,
+
+      // Your application's url
+      targetHost: process.env.APP_URL,
+
+      // An arbitrary string, whatever you'd like to identify these tests by
+      engine: 'mocha+selenium',
+
+      // devtoolsUrl: 'http://localhost:9222',
+      webSocketDebuggerUrl,
     });
+
+    await drill.ready();
+    await drill.startSession();
+  },
+  async beforeEach() {
+    this.timeout(0); // for debug only
+    const testName = getTestName(this.currentTest);
+    await drill.startTest(testName);
   },
   async afterEach() {
-    const endTs = Date.now();
-    //--------------------------------- 4
-    const res = await axios.post(`${process.env.CHROMESTICK_URL}/sessions/${cdpSessionId}/profiler/take-precise-coverage`);
-    const coverage = res.data?.result;
-    const testName = getTestName(this.currentTest);
-    await drillBackend.addJsCoverage({ coverage, testName });
-    const { state, duration } = this.currentTest; // TODO , wallClockStartedAt is missing?
-    drillBackend.addTest(testName, convertTestState(state) as any, endTs - duration, duration);
-  },
-  // runs FOR EVERY FILE in parallel mode, and ONCE in serial mode
-  async afterAll() {
-    await axios.post(`${process.env.CHROMESTICK_URL}/sessions/${cdpSessionId}/profiler/stop-precise-coverage`);
-    await axios.post(`${process.env.CHROMESTICK_URL}/sessions/${cdpSessionId}/profiler/disable`);
-    await drillBackend.finish();
+    this.timeout(0); // for debug only
 
-    await global.driver.close();
+    let start, end;
+    const isSkippedTest = this.currentTest.isPending(); // mocha's oddities
+    if (isSkippedTest) {
+      end = 0;
+      start = 0;
+    } else {
+      end = Date.now();
+      start = end - this.currentTest.duration;
+    }
+    const result = mapMochaTestToDrillTestResult(this.currentTest);
+    const testName = getTestName(this.currentTest);
+    await drill.stopTest(testName, result, start, end);
+  },
+  async afterAll() {
+    this.timeout(0); // for debug only
+    await drill.stopSession();
+    await global.driver.quit();
   },
 };
 
-// runs ONLY ONCE both in parallel and serial mode
+// ---- HELPER FUNCTIONS ---- (format test results data for Drill4J)
+function getTestName(currentTest) {
+  const testNameSeparator = ' ';
+  const parentName = getParentNameChain(currentTest)
+    .filter(x => x)
+    .reverse()
+    .join(testNameSeparator);
+  return `${parentName}${testNameSeparator}${currentTest.title}`;
+}
+
+function getParentNameChain(currentTest) {
+  const res = [];
+  let ptr = currentTest.parent;
+  res.push(ptr.title);
+  while (ptr.parent) {
+    ptr = ptr.parent;
+    res.push(ptr.title);
+  }
+  return res;
+}
+
+function mapMochaTestToDrillTestResult(test: Runnable): any {
+  if (test.isPassed()) return 'PASSED';
+  if (test.isFailed()) return 'FAILED';
+  if (test.isPending()) return 'SKIPPED';
+  return 'UNKNOWN';
+}
+// function mapMochaTestToDrillTestResult(test: Runnable): AdminAPI.TestResult {
+//   if (test.isPassed()) return AdminAPI.TestResult.PASSED;
+//   if (test.isFailed()) return AdminAPI.TestResult.FAILED;
+//   if (test.isPending) return AdminAPI.TestResult.SKIPPED;
+//   return AdminAPI.TestResult.UNKNOWN;
+// }
+
+// ---- TESTS SETUP ---- (got nothing to do with Drill4J)
 export async function mochaGlobalSetup() {
   await startSeleniumServer();
 }
 
-// runs ONLY ONCE both in parallel and serial mode
 export async function mochaGlobalTeardown() {
   await stopSeleniumServer();
-}
-
-async function prepareWebdriver() {
-  const chromeCapabilities = Capabilities.chrome();
-  chromeCapabilities.set('chromeOptions', {
-    //--------------------------------- 2
-    args: ['--headless', `--remote-debugging-port=${process.env.CDP_PORT}`, '--remote-debugging-address=0.0.0.0'],
-  });
-
-  return new Builder().forBrowser('chrome').usingServer(process.env.SELENIUM_HUB_URL).withCapabilities(chromeCapabilities).build();
 }
 
 async function startSeleniumServer() {
@@ -114,49 +164,10 @@ function sleep(ms) {
   return new Promise(res => setTimeout(res, ms));
 }
 
-/**
- * Converts Cypress test status to Drill4J test status
- *
- * [reference](https://docs.cypress.io/guides/core-concepts/writing-and-organizing-tests#Test-statuses)
- *
- * TBD: afterEach is not called for skipped & pending tests, might as well omit these?
- * TBD: UNKNOWN status
- */
-function convertTestState(state) {
-  switch (state) {
-    case 'passed':
-      return 'PASSED';
-    case 'failed':
-      return 'FAILED';
-    case 'pending':
-      return 'SKIPPED';
-    case 'skipped':
-      return 'SKIPPED';
-    default:
-      return 'UNKNOWN';
+declare global {
+  namespace NodeJS {
+    interface Global {
+      driver: WebDriver;
+    }
   }
-}
-
-function getTestName(currentTest) {
-  // Cypress assumes test name separator to be ' '
-  // one might change it to the desired character
-  // e.g. to display "better" names in Drill4J Admin Panel
-  // but then it __have__ to be replaced in test2run.sh before feeding test names to cypress-grep
-  const testNameSeparator = ' ';
-  const parentName = getParentNameChain(currentTest)
-    .filter(x => x)
-    .reverse()
-    .join(testNameSeparator);
-  return `${parentName}${testNameSeparator}${currentTest.title}`;
-}
-
-function getParentNameChain(currentTest) {
-  const res = [];
-  let ptr = currentTest.parent;
-  res.push(ptr.title);
-  while (ptr.parent) {
-    ptr = ptr.parent;
-    res.push(ptr.title);
-  }
-  return res;
 }
